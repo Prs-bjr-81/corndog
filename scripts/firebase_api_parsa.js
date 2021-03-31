1b8debcb79465b1b2e76d0329a10f94b713ed390
Parsa Boojari <pboojari@my.bcit.ca>
2021-03-30 23:43:36 -0700

    part one done

--- scripts/firebase_api_parsa.js
+++ scripts/firebase_api_parsa.js
@@ -4,13 +4,15 @@
 //--------------------------------------------------- 
 
 // Your web app's Firebase configuration
+
+
 var firebaseConfig = {
-  apiKey: 
-  authDomain: 
-  projectId: 
-  storageBucket: 
-  messagingSenderId: 
-  appId:
+  apiKey: "AIzaSyCMgzOWwpyVq511UCuLkjV9TRfurXdQ1pc",
+  authDomain: "garlic-5d0c6.firebaseapp.com",
+  projectId: "garlic-5d0c6",
+  storageBucket: "garlic-5d0c6.appspot.com",
+  messagingSenderId: "312445669448",
+  appId: "1:312445669448:web:09e3691555f8558f63887d"
 };
 
 // Initialize Firebase
